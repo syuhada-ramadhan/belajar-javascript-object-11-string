70870833cf2a4795a1c409821e502e35b62addc4
syuhada-ramadhan <suhadahada18@gmail.com>
2022-01-06 10:24:21 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// startsWith()
+// const namaHewan = "Kambing Etawa";
+// mulai = namaHewan.startsWith("b", 3);
+// console.log(mulai);
+
+// includes()
+// const namaHewan = "Kambing Etawa";
+// karakterB = namaHewan.includes("b", 4);
+// console.log(karakterB);
+
+// lastIndexOf()
+// const namaHewan = "Kambing Etawa";
+// karakterB = namaHewan.lastIndexOf("b", 4);
+// console.log(karakterB);
+
+// toUpperCase()
+const namaHewan = "Kambing Etawa";
+hurufBesar = namaHewan.toUpperCase();
+console.log(hurufBesar);
